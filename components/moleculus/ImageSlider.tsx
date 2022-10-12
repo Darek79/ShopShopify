6ad53d01c51afb528f6151b7a8cf8a692a1f2de1
Darek79 <darius.kloda@gmail.com>
2022-10-12 06:06:34 +0200

    mobile-slider

--- components/moleculus/ImageSlider.tsx
+++ components/moleculus/ImageSlider.tsx
@@ -24,16 +24,20 @@ export default function ImageSlider(): JSX.Element {
         <div className="pb-10 relative">
             <AnimatePresence initial={false}>
                 <LazyMotionWrapper>
-                    <m.div style={{ x }} key="image-slider" className="flex gap-4 w-[calc((100%/3.2)*8)]">
+                    <m.div
+                        style={{ x }}
+                        key="image-slider"
+                        className="flex md:gap-4 w-[calc((100%/1)*8)] md:w-[calc((100%/3.2)*8)]"
+                    >
                         {[10, 20, 30, 40, 50, 60, 70, 80].map(el => (
-                            <div key={el} className="w-[calc(100%/3.2)] bg-blue-200" ref={sliderRef}>
+                            <div key={el} className="w-full md:w-[calc(100%/3.2)] bg-blue-200" ref={sliderRef}>
                                 <Card key={el} href="#" h2Content="Crew neck sweater" pContent="50" />
                             </div>
                         ))}
                     </m.div>
                 </LazyMotionWrapper>
             </AnimatePresence>
-            <div className="absolute hidden md:flex justify-between top-[calc(50%-10px)] w-full z-10">
+            <div className="absolute flex justify-between top-[calc(50%-10px)] w-full z-10">
                 <button
                     type="button"
                     className="border-b-2 border-l-2 w-8 h-8 rotate-45 translate-x-3 md:translate-x-6"
